60c1368260c3aaeb36db8bac642c57a266f42535
Harsh Patel <pharsh58@gmail.com>
2023-10-20 00:36:23 -0400

    test fix for mobile 2

--- rangoli.js
+++ rangoli.js
@@ -138,19 +138,3 @@ function rgbToHex(r,g,b) {
 
 
 hideButtons(); // initially hidden
-
-/* prevents the mobile browser from processing some default
- * touch events, like swiping left for "back" or scrolling
- * the page.
- */
-function touchStarted(){
-  return false;
-}
-
-function touchMoved(){
-  return false;
-}
-
-function touchEnded(){
-  return false;
-}
